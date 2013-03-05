f24f783885f7677dea52fd0461d8a7e288126c47
DerTeufel <dominik-kassel@gmx.de>
2013-03-05 17:35:22 +0100

    crespo.sh: better organized build script
    Change-Id: I064c9f340ab934c01e717bd41be8cc43252156bb

--- crespo.sh
+++ crespo.sh
@@ -1,46 +1,39 @@
 #!/bin/sh
 
-if [ "$1" = "novoodoo" ]; then
+build ()
+{
+    local target=$1
+    echo "Building $target"
+	REL=crespo_devil_$(date +%Y%m%d)
+	make -j4
+
+	echo "copying modules ..."
+	find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
+	echo "making zip ..."
+
+	rm -rf release/kernel/Zimage
+	mv arch/arm/boot/zImage release/kernel/
+	cd release/
+	zip -q -r "${REL}_$target.zip" kernel META-INF system || exit 1
+	cd ..
+}
+
 make crespo_defconfig
+
+if [ "$1" = "novoodoo" ] || [ "$1" = "" ] ; then
 echo "no voodoo"
-REL=crespo_devil_$(date +%Y%m%d)
 sed -i 's/^.*FB_VOODOO.*$//' .config
 echo 'CONFIG_FB_VOODOO=n
 CONFIG_FB_VOODOO_DEBUG_LOG=n' >> .config
-
-make -j4
-
-echo "copying modules ..."
-find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
-echo "making zip ..."
-
-rm -rf release/kernel/Zimage
-mv arch/arm/boot/zImage release/kernel/
-cd release/
-zip -q -r "${REL}.zip" kernel META-INF system || exit 1
-cd ..
+build DC
 fi
 
+
 if [ "$1" = "voodoo" ] ; then
-make crespo_defconfig
 echo "voodoo"
-REL=crespo_devil_vc_$(date +%Y%m%d)
 sed -i 's/^.*FB_VOODOO.*$//' .config
 echo 'CONFIG_FB_VOODOO=y
 CONFIG_FB_VOODOO_DEBUG_LOG=n' >> .config
-
-make -j4
-
-echo "copying modules ..."
-find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
-echo "making zip ..."
-
-rm -rf release/kernel/Zimage
-mv arch/arm/boot/zImage release/kernel/
-cd release/
-zip -q -r "${REL}.zip" kernel META-INF system || exit 1
-cd ..
+build VC
 fi
-
-
 
