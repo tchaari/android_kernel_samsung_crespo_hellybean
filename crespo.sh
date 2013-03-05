fab540eae119b5fdebc7e6b033af4479be629ecf
DerTeufel <dominik-kassel@gmx.de>
2013-03-05 13:10:06 +0100

    crespo.sh: small changes
    Change-Id: I96703f7e004e39a1f6caf844f64ff1e20ebadf0a

--- crespo.sh
+++ crespo.sh
@@ -10,12 +10,15 @@ CONFIG_FB_VOODOO_DEBUG_LOG=n' >> .config
 
 make -j4
 
+echo "copying modules ..."
 find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
+echo "making zip ..."
 
 rm -rf release/kernel/Zimage
 mv arch/arm/boot/zImage release/kernel/
-cd release/kernel
+cd release/
 zip -q -r "${REL}.zip" kernel META-INF system || exit 1
+cd ..
 fi
 
 if [ "$1" = "voodoo" ] ; then
@@ -36,6 +39,7 @@ rm -rf release/kernel/Zimage
 mv arch/arm/boot/zImage release/kernel/
 cd release/
 zip -q -r "${REL}.zip" kernel META-INF system || exit 1
+cd ..
 fi
 
 
